a4131dcfd295544f2c68482635a91a6f590df531
Mark Halls <mark.halls@gmail.com>
2019-12-02 16:25:29 -0700

    Add express listening on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+
+const port = 4000;
+
+const server = express();
+server.use(express.json());
+
+server.listen(port, () => console.log(`\n **API running on port ${port}** \n`));
