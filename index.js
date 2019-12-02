4f35b719cb47ed6f91de6cd4a1e1247244e3b55b
Mark Halls <mark.halls@gmail.com>
2019-12-02 16:59:21 -0700

    Check if user exist and can create new users

--- index.js
+++ index.js
@@ -25,17 +25,42 @@ server.get(`/api/users/:id`, (req, res) => {
   const id = req.params.id;
 
   db.findById(id)
-    .then(user => res.status(200).json(user))
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
     .catch(err => {
       console.error(`error on GET /api/users/:id`, err);
       res.status(500).json({
-        errorMessage: `error getting user with id ${id} from the database`
+        errorMessage: `error getting the requested user from the database`
       });
     });
 });
 
 // add new user at /api/users
-server.post(`/api/users`, (req, res) => {});
+server.post(`/api/users`, (req, res) => {
+  const userData = req.body;
+
+  if (!(req.body.name && req.body.bio)) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert(userData)
+      .then(user => res.status(201).json(user))
+      .catch(err => {
+        console.error(`error on POST /api/users`, err);
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
 
 // delete user by id at /api/users/:id
 server.delete(`/api/users/:id`, (req, res) => {});
