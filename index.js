94dd66cb9d5dd22c062d2ced155a2edb53e15971
Mark Halls <mark.halls@gmail.com>
2019-12-02 16:33:08 -0700

    Create placeholder routes

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ const port = 4000;
 const server = express();
 server.use(express.json());
 
+// get users at /api/users
+server.get(`/api/users`, (req, res) => {});
+
+// get user by id at at /api/users/:id
+server.get(`/api/users/:id`, (req, res) => {});
+
+// add new user at /api/users
+server.post(`/api/users`, (req, res) => {});
+
+// delete user by id at /api/users/:id
+server.delete(`/api/users/:id`, (req, res) => {});
+
+// update user at /api/users/:id
+server.put(`/api/users/:id`, (req, res) => {});
+
 server.listen(port, () => console.log(`\n **API running on port ${port}** \n`));
